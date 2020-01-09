b77a7c9a6f89cec93b050afb7959232f5a5c8cbc
Adam Tetelman <supertetelman@gmail.com>
2020-01-08 17:10:58 -0800

    Get verification working end to end on generic system

--- scripts/k8s_verify_gpu.sh
+++ scripts/k8s_verify_gpu.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# This script is meant to be run after a clean K8S deployment (it can also be run later for debugging)
+# It will get a count of all the GPU in the cluster and attempt to run a job against each one
+# Check the output and verify the number of nodes and GPUs is as expected
+# TODO: This script should be wrapped by Ansible to verify that the output of nvidia-smi on each node matches K8S
 
 CLUSTER_VERIFY_NS=cluster-gpu-verify
 
@@ -12,8 +16,8 @@ TESTS_DIR=$ROOT_DIR/tests
 job_name=$(cat $TESTS_DIR/cluster-gpu-test-job.yml | grep -A1 metadata | awk '{print $2}')
 echo "job_name=$job_name"
 
-name_gpu_node=$(kubectl -n ${CLUSTER_VERIFY_NS} get nodes | grep gpu0 | awk '{print $1}')
-echo "number of GPU nodes: $number_gpu_nodes"
+number_gpu_nodes=$(kubectl describe nodes | sed -n -e '/^Capacity/,/Allocatable/p' | grep nvidia.com/gpu: | wc -l)
+echo "number_gpu_nodes=$number_gpu_nodes"
 
 total_gpus=$(kubectl -n ${CLUSTER_VERIFY_NS} describe nodes  | grep -A7 Capacity | grep nvidia.com/gpu | awk '{print $2}')
 echo "total_gpus=$total_gpus"
@@ -29,12 +33,11 @@ echo "executing ..."
 kubectl -n ${CLUSTER_VERIFY_NS} create -f $TESTS_DIR/cluster-gpu-test-job.yml > /dev/null
 sleep 15
 
-pods_output=$(kubectl -n ${CLUSTER_VERIFY_NS} get pods | awk '$1 ~/nvidia-smi/ && $3 ~/Completed/ {print $1}' )
+pods_output=$(kubectl -n ${CLUSTER_VERIFY_NS} get pods | grep ${job_name} | awk '$3 ~/Completed/ {print $1}' )
 string_array=($pods_output)
 number_pods=${#string_array[@]}
 
 echo "number_pods: ${number_pods}"
-
 if [ $number_pods -lt $total_gpus ]
 then
     echo "GPU driver test failed, use 'kubectl -n ${CLUSTER_VERIFY_NS} describe nodes' to check GPU driver status"
@@ -50,3 +53,6 @@ while [ $i -le $total_gpus ]; do
 done
 
 kubectl delete ns ${CLUSTER_VERIFY_NS}
+echo "Number of Nodes: ${number_gpu_nodes}"
+echo "Number of GPUs: ${total_gpus}"
+echo "${number_pods} / ${total_gpus} GPU Jobs COMPLETED"
